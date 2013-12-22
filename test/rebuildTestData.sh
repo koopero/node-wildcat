35d2d6b12688647760aead531035ee90eb420cc4
koopero <koopero@gmail.com>
2013-12-22 00:41:47 -0800

    Dynamically generated test data

--- test/rebuildTestData.sh
+++ test/rebuildTestData.sh
@@ -1,10 +1,47 @@
 #!/bin/sh
 
-cd data/link
-rm toGif
-ln -s ../image/gif toGif
-cd ../..
+rm -rf data
 
-ffmpeg -ar 96000 -ac 2 -f s16le -i /dev/zero -t 0.125 -y data/audio/silence.wav 2> /dev/null
-ffmpeg -ar 96000 -ac 2 -f s16le -i /dev/zero -t 0.125 -y data/audio/silence.mp3 2> /dev/null
-ffmpeg -ar 96000 -ac 2 -f s16le -i /dev/zero -t 0.125 -y data/audio/silence.ogg 2> /dev/null
+mkdir data
+
+cd data
+
+touch emptyFile
+touch .ignoreThisFile
+
+mkdir image
+cd image
+convert -size 100x300 gradient:'#FFF-#0FF' -rotate 90 \
+          -alpha set -virtual-pixel Transparent +distort Polar 49 +repage \
+          -rotate 90 -set colorspace HSB -colorspace RGB \
+          PNG:png
+
+convert png GIF:gif
+convert png JPEG:jpeg
+convert png TGA:targa
+cd ..
+
+
+mkdir link
+cd link
+ln -s ../image/gif toGif
+cd ..
+
+
+
+mkdir audio
+cd audio
+ffmpeg -ar 96000 -ac 2 -f s16le -i /dev/zero -t 0.125 -y silence.wav 2> /dev/null
+ffmpeg -ar 96000 -ac 2 -f s16le -i /dev/zero -t 0.125 -y silence.mp3 2> /dev/null
+ffmpeg -ar 96000 -ac 2 -f s16le -i /dev/zero -t 0.125 -y silence.ogg 2> /dev/null
+ffmpeg -ar 96000 -ac 2 -f s16le -i /dev/zero -t 0.125 -y silence.flac 2> /dev/null
+cd ..
+
+
+mkdir text
+cd text
+echo "foobar" > foobar
+
+
+
+cd ..
